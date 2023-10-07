e4e4e21d3c0ba4a7fa3ea3340862b35f191b637c
kylead10 <adiokayode111@gmail.com>
2023-10-07 20:29:24 +0100

    fixin login error

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h2>you have logged in</h2>
+    </div>
+  );
 };
 
 export default Login;
