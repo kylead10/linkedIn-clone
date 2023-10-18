743d445529bb646fe367b2431b51b0c5e53f5506
kylead10 <adiokayode111@gmail.com>
2023-10-18 20:46:15 +0100

    routing home and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
+import Header from './components/Header';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Login />} />
+        <Route path='/home' element={<Header /><Home />} />
       </Routes>
     </div>
   );
