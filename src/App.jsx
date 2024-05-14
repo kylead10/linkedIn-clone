aac031a9b62e46ea5e5c3b5865868214a0782f6d
ADIO Kayode <adiokayode111@gmail.com>
2024-05-14 20:15:37 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/home' element={<Header />} />
+        
         <Route path='/home' element={<Home />} />
       </Routes>
     </div>
